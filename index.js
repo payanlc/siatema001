ca3a84610bdd33e15de1d9b74c68832df3a4521b
Lauro Carlos Payán Reyes <payanlc@yahoo.com.mx>
2025-03-19 18:39:27 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,101 +1,84 @@
-const express=require("express")
-const mysql=require("mysql")
+const express=require("express");
+const mongoose=require("mongoose");
+const Product=require("./models/product.model.js");
 const app=express();
-const port=3000;
 
-const conexion=mysql.createConnection( {
-     host:"localhost",
-     user:"root",
-     password:"",
-     database:"datos"
-});
 
-app.set("view engine","ejs")
-app.use(express.urlencoded(extended=false))
 app.use(express.json())
-// app.use(express.static('public'))
-conexion.connect((err)=>{
-    if (err) { 
-        throw err;
-    }
-    else {
-        console.log(" Conectado exitosamente ")
-    }
-    
+app.use(express.urlencoded({extended:false}));
+mongoose.connect("mongodb+srv://admin:5tzwOmi8FGUwj9uR@backenddb.sut3l.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB")
+.then (()=>{
+    console.log("Conectado exitosamente");
 })
-
-app.listen(port,()=>{
-    console.log('Server ejecutando en http://localhost:'+port);    
-})
-
-app.get('/',(req,res)=>{
-    const listac="SELECT * FROM clients ";
-    conexion.query(listac,(error,result)=>{
-        if (error) throw error;     
-        res.render('index',{clientes:result});  
-    });
-    
+.catch(()=>{
+    console.log("Fallo en la conexion a la ND")
 });
 
-app.get("/edita/:id",(req,res)=>{
-    const {id}=req.params;
-    const busca="SELECT * FROM clients WHERE id="+id;
-    conexion.query(busca,(err,result)=>{
-        if (err) throw err;
-        if (result.length>0) {
-            const nombre = result[0].nombre;
-            const telefono = result[0].telefono
-            const direccion=result[0].direccion
-            const estado =result[0].estado
-            const municipio =result[0].municipio
-            const pago =result[0].pago
-           res.render("edita",{id:id,nombre:nombre,direccion:direccion,telefono:telefono,estado:estado,municipio:municipio,pago:pago})
-           console.log(result)
+app.listen(3000, ()=> {
+    console.log(" Server corriendo en http://localhost:3000");
+
+});
+
+app.get("/",(req,res)=>{
+    res.send("HOLA  desde API desde Node")
+})
+
+app.post("/api/products", async (req,res)=>{
+   try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+   } catch (error) {
+    res.status(500).json({message:error.message})
+   }
+});
+
+app.get('/api/products',async(req,res)=>{
+    try {
+        const products= await Product.find({});
+        res.status(200).json(products);
+        
+    } catch (error) {
+        res.status(500).json({message:error.message});
+    }
+});
+
+app.get('/api/products/:id',async(req,res)=>{
+    try {
+        const {id}=req.params;
+        const product= await Product.findById(id);
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({message:error.message});
+    }
+});
+
+app.put('/api/products/:id',async (req,res)=>{
+    try {
+        const {id} = req.params;
+        const product = await Product.findById(id);
+       
+        if (!product){
+            return res.status(404).json({message:"Producto no encontrado"});
+        } else {
+            const updatedPrduct= await Product.findByIdAndUpdate(id,req.body);
+            res.status(200).json(updatedPrduct);
+        
         }
-    })
+    } catch (error) {
+        res.status(500).json({message:error.message});
+    }
+
 });
 
-app.get("/borra/:id",(req,res)=>{
-    const {id}=req.params;
-    const busca= `DELETE FROM clients WHERE  id='${id}' `;
-    conexion.query(busca,(err,result)=>{
-        if (err) throw err;
-        if (result.length>0) [
-           console.log(result)
-        ]
-    })
-    res.redirect("/")
+app.delete('/api/products/:id',async(req,res)=>{
+    try {
+        const {id}=req.params;
+        const product= await Product.findByIdAndDelete(id);
+        if (!product){
+            return res.status(404).json({message:"Producto no encontrado"});
+        }
+        res.status(200).json({message:"Producto eliminado exitsamente"});
+    } catch (error) {
+        res.status(500).json({message:error.message});
+    }
 });
-
-app.post('/inserta',(req,res)=>{
-
-    const nombre = req.body.nombre;
-    const telefono = req.body.telefono
-    const direccion= req.body.direccion
-    const estado = req.body.estado
-    const municipio = req.body.municipio
-    const pago = req.body.pago
-
-    conexion.query("INSERT INTO clients SET ?",{nombre:nombre,telefono:telefono,direccion:direccion,estado:estado,municipio:municipio,pago:pago},(error,result)=>{
-        if (error) throw error;     
-        res.redirect('/');  
-    });
-    
-});
-
-app.post('/insertar',(req,res)=>{
-    const id=req.body.id
-    const nombre = req.body.nombre;
-    const telefono = req.body.telefono
-    const direccion= req.body.direccion
-    const estado = req.body.estado
-    const municipio = req.body.municipio
-    const pago = req.body.pago
-    conexion.query("UPDATE clients SET ? WHERE ?;",[{nombre:nombre,telefono:telefono,direccion:direccion,estado:estado,municipio:municipio,pago:pago},{id:id}],(error,result)=>{
-        if (error) throw error;     
-        res.redirect('/');  
-        console.log("Actualizado")
-    });
-    
-});
-
